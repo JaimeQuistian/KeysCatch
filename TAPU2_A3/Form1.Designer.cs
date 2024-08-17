e050c7ce0b4212b52884f40596d93e46d5d170c5
JEQS <104605294+JaimeQuistian@users.noreply.github.com>
2024-08-17 17:00:16 -0600

    FirstVersion
    The base code is finished, and the GUI needs to be improved.

---
+++ TAPU2_A3/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace TAPU2_A3
+{
+    partial class frmAtraparLetras
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblPuntos = new System.Windows.Forms.Label();
+            this.lblNumeroPuntaje = new System.Windows.Forms.Label();
+            this.lblInformación = new System.Windows.Forms.Label();
+            this.lblLetra = new System.Windows.Forms.Label();
+            this.tmrMostrarLetras = new System.Windows.Forms.Timer(this.components);
+            this.lblTeclaUsuario = new System.Windows.Forms.Label();
+            this.tmrEvaluacion = new System.Windows.Forms.Timer(this.components);
+            this.lblInformacion2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblPuntos
+            // 
+            this.lblPuntos.AutoSize = true;
+            this.lblPuntos.Font = new System.Drawing.Font("MV Boli", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPuntos.Location = new System.Drawing.Point(12, 370);
+            this.lblPuntos.Name = "lblPuntos";
+            this.lblPuntos.Size = new System.Drawing.Size(55, 17);
+            this.lblPuntos.TabIndex = 0;
+            this.lblPuntos.Text = "Puntos:";
+            // 
+            // lblNumeroPuntaje
+            // 
+            this.lblNumeroPuntaje.AutoSize = true;
+            this.lblNumeroPuntaje.Font = new System.Drawing.Font("MV Boli", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumeroPuntaje.ForeColor = System.Drawing.Color.Blue;
+            this.lblNumeroPuntaje.Location = new System.Drawing.Point(73, 370);
+            this.lblNumeroPuntaje.Name = "lblNumeroPuntaje";
+            this.lblNumeroPuntaje.Size = new System.Drawing.Size(17, 17);
+            this.lblNumeroPuntaje.TabIndex = 1;
+            this.lblNumeroPuntaje.Text = "0";
+            // 
+            // lblInformación
+            // 
+            this.lblInformación.AutoSize = true;
+            this.lblInformación.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInformación.Location = new System.Drawing.Point(-3, 112);
+            this.lblInformación.Name = "lblInformación";
+            this.lblInformación.Size = new System.Drawing.Size(203, 23);
+            this.lblInformación.TabIndex = 2;
+            this.lblInformación.Text = "Presiona la siguiente tecla:";
+            // 
+            // lblLetra
+            // 
+            this.lblLetra.AutoSize = true;
+            this.lblLetra.Font = new System.Drawing.Font("MV Boli", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLetra.ForeColor = System.Drawing.Color.Red;
+            this.lblLetra.Location = new System.Drawing.Point(-5, 177);
+            this.lblLetra.Name = "lblLetra";
+            this.lblLetra.Size = new System.Drawing.Size(0, 34);
+            this.lblLetra.TabIndex = 3;
+            // 
+            // tmrMostrarLetras
+            // 
+            this.tmrMostrarLetras.Enabled = true;
+            this.tmrMostrarLetras.Interval = 2000;
+            this.tmrMostrarLetras.Tick += new System.EventHandler(this.tmrMostrarLetras_Tick);
+            // 
+            // lblTeclaUsuario
+            // 
+            this.lblTeclaUsuario.AutoSize = true;
+            this.lblTeclaUsuario.ForeColor = System.Drawing.Color.Blue;
+            this.lblTeclaUsuario.Location = new System.Drawing.Point(533, 374);
+            this.lblTeclaUsuario.Name = "lblTeclaUsuario";
+            this.lblTeclaUsuario.Size = new System.Drawing.Size(10, 13);
+            this.lblTeclaUsuario.TabIndex = 4;
+            this.lblTeclaUsuario.Text = "-";
+            // 
+            // tmrEvaluacion
+            // 
+            this.tmrEvaluacion.Enabled = true;
+            this.tmrEvaluacion.Interval = 2000;
+            this.tmrEvaluacion.Tick += new System.EventHandler(this.tmrEvaluacion_Tick);
+            // 
+            // lblInformacion2
+            // 
+            this.lblInformacion2.AutoSize = true;
+            this.lblInformacion2.Location = new System.Drawing.Point(443, 374);
+            this.lblInformacion2.Name = "lblInformacion2";
+            this.lblInformacion2.Size = new System.Drawing.Size(84, 13);
+            this.lblInformacion2.TabIndex = 5;
+            this.lblInformacion2.Text = "Haz presionado:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(-3, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(562, 92);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Instrucciones: Precionar la tecla que aparece en pantalla para que no llegue \r\nha" +
+    "sta el final de la linea.\r\n\r\nSuerte . . .\r\n";
+            // 
+            // frmAtraparLetras
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(559, 400);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblInformacion2);
+            this.Controls.Add(this.lblTeclaUsuario);
+            this.Controls.Add(this.lblLetra);
+            this.Controls.Add(this.lblInformación);
+            this.Controls.Add(this.lblNumeroPuntaje);
+            this.Controls.Add(this.lblPuntos);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(575, 439);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(575, 439);
+            this.Name = "frmAtraparLetras";
+            this.ShowIcon = false;
+            this.Text = "KeysCatch()";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.frmAtraparLetras_KeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPuntos;
+        private System.Windows.Forms.Label lblNumeroPuntaje;
+        private System.Windows.Forms.Label lblInformación;
+        private System.Windows.Forms.Label lblLetra;
+        private System.Windows.Forms.Timer tmrMostrarLetras;
+        private System.Windows.Forms.Label lblTeclaUsuario;
+        private System.Windows.Forms.Timer tmrEvaluacion;
+        private System.Windows.Forms.Label lblInformacion2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TAPU2_A3/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TAPU2_A3
+{
+    public partial class frmAtraparLetras : Form
+    {
+        // ---------------- Clase global ----------------
+        Random letraAleatoria = new Random();
+        // ---------------- Variable global ----------------
+        int puntaje = 0;
+        int cambioDeVelocidad = 0;
+        // ---------------- Cuadro de DialogResult global ----------------
+        DialogResult reinicio = new DialogResult();
+
+        // ----------------- Constructor ----------------
+        public frmAtraparLetras()
+        {
+            InitializeComponent();
+        }
+
+        // ----------------- Eventos -----------------
+
+        //Creación de un evento tick para el timer donde mostrará las letras que aparecerán en el formulario
+        private void tmrMostrarLetras_Tick(object sender, EventArgs e)
+        {
+            //Se crea una variave char donde se guardarán las letras por medio de una clase random donde trendremos que
+            //realizar un "cast" para convertir los numeros generados en letras por medio de su codigo ASCII (las letras
+            //en el formulario apaereceran en mayusculas)
+            char conversionALetras = (char)(letraAleatoria.Next(65, 90));
+            //Se estarán concatenando de manera sucesiva las letras y se grardaran en el label lblLetra en su propiedad 
+            //text (se deberá convertir la variable de tipo char a string)
+            lblLetra.Text += conversionALetras.ToString();
+            //Al inciarse aletoriamente las letras tendran una primera velocidad por default hasta que llegue a 5 ticks
+            //por medio deun contador llamado cambio de velocidad que despues de cada tick incrementará su valor
+            cambioDeVelocidad += 1;
+            //Cuando el cntador cambio de velocidad llegue a 5 las letras cambiarán su velocidad de aparición
+            if (cambioDeVelocidad == 5)
+            {
+                tmrMostrarLetras.Interval = 1000;
+                tmrEvaluacion.Interval = 1000;
+            }
+            //Cuando el cntador cambio de velocidad llegue a 10 las letras cambiarán su velocidad de aparición
+            if (cambioDeVelocidad == 10)
+            {
+                tmrMostrarLetras.Interval = 900;
+                tmrEvaluacion.Interval = 900;
+            }
+            //Cuando el cntador cambio de velocidad llegue a 15 las letras cambiarán su velocidad de aparición
+            if (cambioDeVelocidad == 15)
+            {
+                tmrMostrarLetras.Interval = 800;
+                tmrEvaluacion.Interval = 800;
+            }
+
+            //Si las letras logran llegar al limite de la parte derecha del formualrio se detendrán, enviando un mensaje
+            //de que el juego terminó y la opcion de poder reinciarlo
+            if (lblLetra.Right >= ClientSize.Width)
+            {
+                //Se apaga el evento para tmrEvaluacion
+                tmrEvaluacion.Enabled = false;
+                //Se pausa el evento de tmrMostrarLEtras
+                tmrMostrarLetras.Stop();
+                //Se reinicia el texto, las letras precionadas y el puntaje
+                lblLetra.Text = "";
+                lblTeclaUsuario.Text = "-";
+                reinicio = MessageBox.Show("Fin del Juego\nReiniciar", "Game Over", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                lblNumeroPuntaje.Text = "0";
+                //Secuencia de evaluación donde sí el usuario decide reiniciar evaluará su respuesta y empezará a
+                //acativar todo nuevamente
+                if (reinicio == DialogResult.Yes)
+                {
+                    //Al reiniciar el juego las letras se iniciarán nuevamente y el contador reinicia su valor en 0
+                    tmrMostrarLetras.Start();
+                    tmrEvaluacion.Enabled = true;
+                    cambioDeVelocidad = 0;
+                }
+            }
+        }
+
+        //Se crea un evento keyPress en el formulario la cual guardará las teclas presionadas por el usuario
+        private void frmAtraparLetras_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //Modificar el lablel lblTeclaUsuario en su propiedad text con la tecla presionada por el usuario
+            //(convertir a string la tecla precionada por el usuario guardada por el evento keyPress)
+            lblTeclaUsuario.Text = Convert.ToString(e.KeyChar);
+        }
+
+        //Se añade otro timer con el evento tick que evalua las teclas presionadas por el usario
+        private void tmrEvaluacion_Tick(object sender, EventArgs e)
+        {
+            //Declaración de una variable utilizada para guardar el valor del label lblLetra en su propiedad text 
+            //antes de que se pueda atualizar
+            string comparacionTextoAnterior = lblLetra.Text;
+            //Creación de una nueva variable para poder eliminar la tecla presionada a tiempo
+            string nuevoTexto;
+            //Igualar la variable nuevoTexto con el valor del lablel lblLetra en su propiedad text
+            nuevoTexto = lblLetra.Text;
+            //Obtener el ultimo valor de la cadena de caracteres para que con el metodo TrimEnd obtener y eliminar la
+            //ultima letra presionada por el usuario
+            nuevoTexto = nuevoTexto.TrimEnd(Convert.ToChar(lblTeclaUsuario.Text));
+            //Asignarle el nuevo texto al label lblLetra nn su propiedad text el nuevo valor sin la letra precionada
+            lblLetra.Text = nuevoTexto;
+            //Secuencia comparatia para determinar si el texto anterior del labl lblLetra guardado en la variable
+            //comparacionTextoAnterior con el valor actual del label lblLetra en su propiedad text
+            if (comparacionTextoAnterior != lblLetra.Text)
+            {
+                //En caso de cumplirse la sentencia se aumentará el valor de la variable puntaje 1 unidad para
+                //determinar un puntaje de teclas correctas, ademas de también modificar el label lblNumeroPuntaje
+                //en su propiedad text con el valor de puntaje (convertir antes a tipo sting lo guardado en la
+                //variable puntaje)
+                puntaje += 1;
+                lblNumeroPuntaje.Text = Convert.ToString(puntaje);
+            }
+        }
+    }
+}
